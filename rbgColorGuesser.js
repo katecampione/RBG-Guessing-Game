0058ec39b25beb7ca232a5f9664951d32220e95b
Kate Campione <katecampione@gmail.com>
2019-08-29 16:39:46 -0600

    added picked color message

--- rbgColorGuesser.js
+++ rbgColorGuesser.js
@@ -9,6 +9,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 for(var i = 0; i < squares.length; i++){
@@ -21,9 +22,10 @@ for(var i = 0; i < squares.length; i++){
     //compare color to pickedColor
     var clickedColor = this.style.backgroundColor;
     if(clickedColor === pickedColor){
-      alert("correct!");
+      messageDisplay.textContent = "Correct!";
     } else {
       this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again"
     }
   });
 }
